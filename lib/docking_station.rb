fc4d98633763b94e82d3f8d4f0689cd649ad9bd3
Alfie Freeman <runticle@gmail.com>
2018-10-02 16:40:54 +0100

    throw Error if user tries to release_bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,7 @@
 class DockingStation
   def release_bike
-    Bike.new
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
   def dock_bike(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,11 +4,18 @@ require 'bike'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bike' do
-    bike = subject.release_bike
-    expect(bike.working?).to eq true
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.release_bike).to eq bike
+    end
+    it 'raises error when there are no bikes' do
+      expect {subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
-    it 'docks bike' do
-      expect(subject).to respond_to :dock_bike
+
+  it 'docks bike' do
+    expect(subject).to respond_to :dock_bike
   end
 end
