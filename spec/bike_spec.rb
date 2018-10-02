b620e6a78e513706303536785c0c2d2efac25263
Alfie Freeman <runticle@gmail.com>
2018-10-02 14:46:27 +0100

    added test to check if bike.working? true and let release_bike create new Bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,6 @@
 require 'docking_station'
+require 'bike'
+
 describe Bike do
   it { is_expected.to respond_to :working?}
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,11 @@
 require 'docking_station'
+require 'bike'
+
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  it 'releases working bike' do
+    bike = subject.release_bike
+    expect(bike.working?).to eq true
+  end
 end
